4ce87ea4ca210546e08317a378f7e308a8ba0fb6
Ellen Peeters <ellen.peeters@dotnetlab.eu>
2021-05-10 10:40:54 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from './components/home/home.component';
+import {PaginaComponent} from './components/pagina/pagina.component';
+import {Child1Component} from './components/child1/child1.component';
+import {Child2Component} from './components/child2/child2.component';
+import {BlogComponent} from './components/blog/blog.component';
+import {Page404Component} from './components/page404/page404.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'pagina', component: PaginaComponent, children: [
+    {path: 'child1', component: Child1Component},
+    {path: 'child2', component: Child2Component},
+  ]},
+  {path: 'blog', component: BlogComponent},
+  {path: '**', component: Page404Component},
+];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { MessageService } from './services/message/message.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoodschapComponent } from './components/boodschap/boodschap.component';
-import { TestComponent } from './components/test/test.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { Page404Component } from './components/page404/page404.component';
+import { Child1Component } from './components/child1/child1.component';
+import { Child2Component } from './components/child2/child2.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoodschapComponent,
-    TestComponent
+    HomeComponent,
+    BlogComponent,
+    Page404Component,
+    Child1Component,
+    Child2Component,
+    PaginaComponent
   ],
   imports: [
     BrowserModule,
